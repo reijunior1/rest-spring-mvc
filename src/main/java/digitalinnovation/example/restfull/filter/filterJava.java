1f200e9b3c033f705991ad67697c6668b6339a9d
reijunior1 <reijunior1@hotmail.com>
2020-06-11 00:03:17 -0300

    Update filterJava.java

--- src/main/java/digitalinnovation/example/restfull/filter/filterJava.java
+++ src/main/java/digitalinnovation/example/restfull/filter/filterJava.java
@@ -1,5 +1,8 @@
 package digitalinnovation.example.restfull.filter;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,11 +10,16 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Map;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-public class FilterJava implements Filter {
+@Component
+public class filterJava implements Filter {
+    private Logger logger = (Logger) LoggerFactory.getLogger(filterJava.class);
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("Chegou no portão do castelo");
         HttpServletRequest req = (HttpServletRequest) request;
         Enumeration<String> headersNames = req.getHeaderNames();
         Map<String, String> mapHeaders = Collections.list(headersNames)
@@ -22,5 +30,6 @@ public class FilterJava implements Filter {
             HttpServletResponse res = (HttpServletResponse) response;
             res.sendError(403);
         }
+        logger.info("Saiu do portão do castelo");
     }
 }
