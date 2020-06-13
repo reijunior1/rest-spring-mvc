62537dd67475680451322685cf54a61c12fd1d55
reijunior1 <reijunior1@hotmail.com>
2020-06-12 21:18:21 -0300

    Aplicando Jackson e Binder no projeto

--- src/main/java/digitalinnovation/example/restfull/enums/Raca.java
+++ src/main/java/digitalinnovation/example/restfull/enums/Raca.java
@@ -1,14 +1,27 @@
 package digitalinnovation.example.restfull.enums;
 
+import java.util.stream.Stream;
+
 public enum Raca {
     HUMANO("humano"),
-    elfo("elfo"),
-    orc("orc"),
-    anao("anao");
-
+    ELFO("elfo"),
+    ORC("orc"),
+    ANAO("anao");
 
     private String value;
+
     Raca(String value) {
         this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static Raca of(String value){
+        return Stream.of(Raca.values())
+                .filter(it -> it.getValue().equals(value))
+                .findFirst()
+                .orElseThrow();
     }
 }
